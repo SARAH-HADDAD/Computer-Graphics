c1e437670d790caa501759fa7b46679cce468619
Haddad Sarah <haddads686@gmail.com>
2023-11-27 19:50:20 +0100

    Trackball camera

--- main.cpp
+++ main.cpp
@@ -6,8 +6,9 @@
 using namespace glm;
 struct STRVertex
 {
-vec3 position;
-vec3 couleur; };
+    vec3 position;
+    vec3 couleur;
+};
 void framebuffer_size_callback(GLFWwindow *window, int width, int height);
 void processInput(GLFWwindow *window);
 
@@ -25,23 +26,22 @@ const unsigned int SCR_HEIGHT = 600;
 //                                  "   gl_Position = MVP * vec4(VertexPosition.xyz, 1.0);\n"
 //                                  "   Color = VertexColor;\n" // Pass the color to the fragment shader
 //                                  "}\0";
-const char *vertexShaderSource = 
-                                "#version 330 core\n"
+const char *vertexShaderSource =
+    "#version 330 core\n"
 
-                                "layout(location = 0) in vec3 VertexPosition;\n"
-                                "layout(location = 1) in vec3 VertexColor;\n"
-                                "uniform mat4 MVP;\n"
+    "layout(location = 0) in vec3 VertexPosition;\n"
+    "layout(location = 1) in vec3 VertexColor;\n"
+    "uniform mat4 MVP;\n"
 
-                                "out vec3 Vcolor;\n"
-                                "void main(){\n"
-                                    "gl_Position = MVP * vec4(VertexPosition, 1);\n"
+    "out vec3 Vcolor;\n"
+    "void main(){\n"
+    "gl_Position = MVP * vec4(VertexPosition, 1);\n"
 
-                                    //gl_Position.xyz = VertexPosition;
-                                    //gl_Position.w = 1.0;
+    // gl_Position.xyz = VertexPosition;
+    // gl_Position.w = 1.0;
 
-                                    "Vcolor = VertexColor;\n"
-                                "}\0";
-
+    "Vcolor = VertexColor;\n"
+    "}\0";
 
 // const char *fragmentShaderSource = "#version 330 core\n"
 //                                    "out vec3 FragColor;\n"
@@ -50,30 +50,30 @@ const char *vertexShaderSource =
 //                                    "   FragColor = vec3(1, 0, 0);\n"
 //                                    "}\n\0";
 
-const char *fragmentShaderSource = 
-                            "#version 330 core\n"
+const char *fragmentShaderSource =
+    "#version 330 core\n"
 
-                            "out vec3 color;\n"
-                            "in vec3 Vcolor;\n"
+    "out vec3 color;\n"
+    "in vec3 Vcolor;\n"
 
-                            "void main()\n"
-                            "{\n"   
-                                "color = Vcolor;   //vec3(1,0,0);\n"
-                            "}\n\0";
+    "void main()\n"
+    "{\n"
+    "color = Vcolor;   //vec3(1,0,0);\n"
+    "}\n\0";
 
 mat4 Model = mat4(1.0f);
 mat4 Projection = perspective(radians(45.0f), 4.0f / 3.0f, 0.1f, 100.0f);
-mat4 View = lookAt(vec3(5,4,7),vec3(-1,0,0),vec3(2,3,2) );  
+mat4 View = lookAt(vec3(5, 4, 7), vec3(-1, 0, 0), vec3(2, 3, 2));
 static double Xpos = 0.0, Ypos = 0.0;
-static void cursor_position(GLFWwindow* window, double x, double y)
+static void cursor_position(GLFWwindow *window, double x, double y)
 {
 
-double diffX;
-diffX=Xpos-x;
-double diffY;
-diffY=y-Ypos;
-Model = rotate(Model,radians(float(diffX)),vec3(0.0f,0.0f,1.0f)); 
-Model = rotate(Model,radians(float(diffY)),vec3(1.0f,0.0f,0.0f));
+    double diffX;
+    diffX = Xpos - x;
+    double diffY;
+    diffY = y - Ypos;
+    Model = rotate(Model, radians(float(diffX)), vec3(0.0f, 0.0f, 1.0f));
+    Model = rotate(Model, radians(float(diffY)), vec3(1.0f, 0.0f, 0.0f));
 }
 int fov = 45;
 void scroll(GLFWwindow *window, double xoffset, double yoffset)
@@ -91,27 +91,30 @@ void scroll(GLFWwindow *window, double xoffset, double yoffset)
     Projection = perspective(radians(static_cast<float>(fov)), 4.0f / 3.0f, 0.1f, 100.0f);
 }
 
-static void key_callback(GLFWwindow* window, int key, int scancode, int action, int mods)
+static void key_callback(GLFWwindow *window, int key, int scancode, int action, int mods)
 {
-if (action == GLFW_PRESS)
-switch (key)
-{
-case GLFW_KEY_R :
-glClearColor(1.0f, 0.0f, 0.0f, 0.5f);
-break;
-case GLFW_KEY_V :
-glClearColor(0.0f, 1.0f, 0.0f, 0.5f);
-break;
-case GLFW_KEY_B :
-glClearColor(0.0f, 0.0f, 1.0f, 0.5f);
-break;
-case GLFW_KEY_ESCAPE:
-glfwSetWindowShouldClose(window, GLFW_TRUE);
-break; default:
-glClearColor(0.0f, 0.0f, 0.0f, 0.5f); }
-else if (action== GLFW_RELEASE){
-    glClearColor(0.0f, 0.0f, 0.0f, 0.5f);
-}
+    if (action == GLFW_PRESS)
+        switch (key)
+        {
+        case GLFW_KEY_R:
+            glClearColor(1.0f, 0.0f, 0.0f, 0.5f);
+            break;
+        case GLFW_KEY_V:
+            glClearColor(0.0f, 1.0f, 0.0f, 0.5f);
+            break;
+        case GLFW_KEY_B:
+            glClearColor(0.0f, 0.0f, 1.0f, 0.5f);
+            break;
+        case GLFW_KEY_ESCAPE:
+            glfwSetWindowShouldClose(window, GLFW_TRUE);
+            break;
+        default:
+            glClearColor(0.0f, 0.0f, 0.0f, 0.5f);
+        }
+    else if (action == GLFW_RELEASE)
+    {
+        glClearColor(0.0f, 0.0f, 0.0f, 0.5f);
+    }
 }
 
 int main()
@@ -195,47 +198,40 @@ int main()
     }
     glDeleteShader(vertexShader);
     glDeleteShader(fragmentShader);
-    
 
-    
-    Model = translate(Model,vec3(-1.0f, 0.0f, 0.0f));
-    Model = scale(Model,vec3(2.5f, 1.5f ,1.0f));
+    Model = translate(Model, vec3(-1.0f, 0.0f, 0.0f));
+    Model = scale(Model, vec3(2.5f, 1.5f, 1.0f));
     mat4 MVP = Projection * View * Model;
     GLuint MatrixID = glGetUniformLocation(shaderProgram, "MVP");
-
-
-
-
 
     // set up vertex data (and buffer(s)) and configure vertex attributes
     // ------------------------------------------------------------------
     STRVertex vertices[] = {
-        //2 triangles de la base :
-        vec3(1.0f, 0.0f, 1.0f),  vec3(0.0f, 0.0f, 1.0f),
+        // 2 triangles de la base :
+        vec3(1.0f, 0.0f, 1.0f), vec3(0.0f, 0.0f, 1.0f),
         vec3(-1.0f, 0.0f, 1.0f), vec3(0.0f, 0.0f, 1.0f),
-        vec3(-1.0f, 0.0f, -1.0f), vec3(0.0f, 0.0f, 1.0f),  //comuns
+        vec3(-1.0f, 0.0f, -1.0f), vec3(0.0f, 0.0f, 1.0f), // comuns
         vec3(-1.0f, 0.0f, -1.0f), vec3(0.0f, 0.0f, 1.0f),
         vec3(1.0f, 0.0f, -1.0f), vec3(0.0f, 0.0f, 1.0f),
-        vec3(1.0f, 0.0f, 1.0f),  vec3(0.0f, 0.0f, 1.0f),     //comuns
+        vec3(1.0f, 0.0f, 1.0f), vec3(0.0f, 0.0f, 1.0f), // comuns
 
-        //ajouter les 4 triangles :
-        vec3(1.0f, 0.0f, 1.0f),  vec3(1.0f, 0.0f, 1.0f),
+        // ajouter les 4 triangles :
+        vec3(1.0f, 0.0f, 1.0f), vec3(1.0f, 0.0f, 1.0f),
         vec3(-1.0f, 0.0f, 1.0f), vec3(1.0f, 0.0f, 1.0f),
-        vec3(0.0f, 1.0f, 0.0f),  vec3(1.0f, 0.0f, 1.0f),    //sommet
+        vec3(0.0f, 1.0f, 0.0f), vec3(1.0f, 0.0f, 1.0f), // sommet
 
-        vec3(-1.0f, 0.0f, 1.0f),  vec3(0.0f, 1.0f, 1.0f),
+        vec3(-1.0f, 0.0f, 1.0f), vec3(0.0f, 1.0f, 1.0f),
         vec3(-1.0f, 0.0f, -1.0f), vec3(0.0f, 1.0f, 1.0f),
-        vec3(0.0f, 1.0f, 0.0f),   vec3(0.0f, 1.0f, 1.0f),    //sommet
+        vec3(0.0f, 1.0f, 0.0f), vec3(0.0f, 1.0f, 1.0f), // sommet
 
         vec3(-1.0f, 0.0f, -1.0f), vec3(1.0f, 0.0f, 0.0f),
         vec3(1.0f, 0.0f, -1.0f), vec3(1.0f, 0.0f, 0.0f),
-        vec3(0.0f, 1.0f, 0.0f),  vec3(1.0f, 0.0f, 0.0f),     //sommet
+        vec3(0.0f, 1.0f, 0.0f), vec3(1.0f, 0.0f, 0.0f), // sommet
 
         vec3(1.0f, 0.0f, -1.0f), vec3(0.0f, 1.0f, 0.0f),
-        vec3(1.0f, 0.0f, 1.0f),  vec3(0.0f, 1.0f, 0.0f),
-        vec3(0.0f, 1.0f, 0.0f),  vec3(0.0f, 1.0f, 0.0f)   //sommet
-        };
-
+        vec3(1.0f, 0.0f, 1.0f), vec3(0.0f, 1.0f, 0.0f),
+        vec3(0.0f, 1.0f, 0.0f), vec3(0.0f, 1.0f, 0.0f) // sommet
+    };
 
     unsigned int VBO, VAO;
     glGenVertexArrays(1, &VAO);
@@ -246,18 +242,16 @@ int main()
     glBindBuffer(GL_ARRAY_BUFFER, VBO);
     glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
 
-    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, sizeof(STRVertex) , (void*)offsetof(STRVertex,position) );
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, sizeof(STRVertex), (void *)offsetof(STRVertex, position));
     glEnableVertexAttribArray(0);
-    glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, sizeof(STRVertex) , (void*)offsetof(STRVertex,couleur) );
+    glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, sizeof(STRVertex), (void *)offsetof(STRVertex, couleur));
     glEnableVertexAttribArray(1);
-    
 
     // note that this is allowed, the call to glVertexAttribPointer registered VBO as the vertex attribute's bound vertex buffer object so afterwards we can safely unbind
     glBindBuffer(GL_ARRAY_BUFFER, 0);
 
     // You can unbind the VAO afterwards so other VAO calls won't accidentally modify this VAO, but we'll do so to keep things a bit more organized
     glBindVertexArray(0);
-
 
     glEnable(GL_DEPTH_TEST);
     glDepthFunc(GL_LESS);
@@ -270,21 +264,25 @@ int main()
     glfwSetKeyCallback(window, key_callback);
     glfwSetCursorPosCallback(window, cursor_position);
     glfwSetScrollCallback(window, scroll);
+    float radius = 5.0f;
+
     while (!glfwWindowShouldClose(window))
     {
 
-glfwGetCursorPos(window, &Xpos, &Ypos);
-std::cout << "La position du curseur : " << Xpos << " , " << Ypos << std::endl;
-
-//if ( glfwGetMouseButton(window, GLFW_MOUSE_BUTTON_LEFT) == GLFW_PRESS)
-//std::cout << " Bouton gauche de la souris appuyÃ© " << std::endl;
-mat4 MVP = Projection * View * Model;
-GLuint MatrixID = glGetUniformLocation(shaderProgram, "MVP");
+        glfwGetCursorPos(window, &Xpos, &Ypos);
+        // std::cout << "La position du curseur : " << Xpos << " , " << Ypos << std::endl;
+        // if ( glfwGetMouseButton(window, GLFW_MOUSE_BUTTON_LEFT) == GLFW_PRESS)
+        // std::cout << " Bouton gauche de la souris appuyÃ© " << std::endl;
+        //  Trackball camera
+        float cameraX = radius * sin(glfwGetTime() );
+        float cameraZ = radius * cos(glfwGetTime() );
+        View = lookAt(vec3(cameraX, 0.0, cameraZ), vec3(0, 0, 0), vec3(0, 1, 0));
+        mat4 MVP = Projection * View * Model;
+        glUniformMatrix4fv(MatrixID, 1, GL_FALSE, &MVP[0][0]);
 
         processInput(window);
 
-
-        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT) ;
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
         // draw our first triangle
         glUseProgram(shaderProgram);
         glBindVertexArray(VAO);
@@ -293,7 +291,7 @@ GLuint MatrixID = glGetUniformLocation(shaderProgram, "MVP");
         glDrawArrays(GL_TRIANGLES, 0, 54);
 
         glfwSwapBuffers(window);
-        
+
         glfwPollEvents();
     }
 
@@ -308,7 +306,6 @@ GLuint MatrixID = glGetUniformLocation(shaderProgram, "MVP");
     glfwTerminate();
     return 0;
 }
-
 
 // process all input: query GLFW whether relevant keys are pressed/released this frame and react accordingly
 // ---------------------------------------------------------------------------------------------------------
