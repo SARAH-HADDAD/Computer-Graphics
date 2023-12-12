f3a9a6e540260bb87e86e36555368ef80bb34f55
Haddad Sarah <haddads686@gmail.com>
2023-12-12 14:40:14 +0100

    texture

--- main.cpp
+++ main.cpp
@@ -5,147 +5,61 @@
 #include "glm/glm/gtx/transform.hpp"
 #include "objload.hpp"
 using namespace glm;
-using namespace std;
-
-using namespace glm;
-using namespace std;
-
-#define _CRT_SECURE_NO_WARNINGS
-
-// Very, VERY simple OBJ loader.
-
-bool loadOBJ(
-    const char *path,
-    std::vector<glm::vec3> &out_vertices,
-    std::vector<glm::vec2> &out_uvs,
-    std::vector<glm::vec3> &out_normals)
-{
-    printf("Loading OBJ file %s...\n", path);
-
-    std::vector<unsigned int> vertexIndices, uvIndices, normalIndices;
-    std::vector<glm::vec3> temp_vertices;
-    std::vector<glm::vec2> temp_uvs;
-    std::vector<glm::vec3> temp_normals;
-
-    FILE *file = fopen(path, "r");
-    if (file == NULL)
-    {
-        printf("Impossible to open the file !\n");
-        getchar();
-        return false;
-    }
-
-    while (1)
-    {
-
-        char lineHeader[128];
-        int res = fscanf(file, "%s", lineHeader);
-        if (res == EOF)
-            break; // EOF
-        if (strcmp(lineHeader, "v") == 0)
-        {
-            glm::vec3 vertex;
-            fscanf(file, "%f %f %f\n", &vertex.x, &vertex.y, &vertex.z);
-            temp_vertices.push_back(vertex);
-        }
-        else if (strcmp(lineHeader, "vt") == 0)
-        {
-            glm::vec2 uv;
-            fscanf(file, "%f %f\n", &uv.x, &uv.y);
-            uv.y = -uv.y;
-            temp_uvs.push_back(uv);
-        }
-        else if (strcmp(lineHeader, "vn") == 0)
-        {
-            glm::vec3 normal;
-            fscanf(file, "%f %f %f\n", &normal.x, &normal.y, &normal.z);
-            temp_normals.push_back(normal);
-        }
-        else if (strcmp(lineHeader, "f") == 0)
-        {
-            std::string vertex1, vertex2, vertex3;
-            unsigned int vertexIndex[3], uvIndex[3], normalIndex[3];
-            int matches = fscanf(file, "%d/%d/%d %d/%d/%d %d/%d/%d\n", &vertexIndex[0], &uvIndex[0], &normalIndex[0], &vertexIndex[1], &uvIndex[1], &normalIndex[1], &vertexIndex[2], &uvIndex[2], &normalIndex[2]);
-            if (matches != 9)
-            {
-                printf("File can't be read \n");
-                return false;
-            }
-            vertexIndices.push_back(vertexIndex[0]);
-            vertexIndices.push_back(vertexIndex[1]);
-            vertexIndices.push_back(vertexIndex[2]);
-            uvIndices.push_back(uvIndex[0]);
-            uvIndices.push_back(uvIndex[1]);
-            uvIndices.push_back(uvIndex[2]);
-            normalIndices.push_back(normalIndex[0]);
-            normalIndices.push_back(normalIndex[1]);
-            normalIndices.push_back(normalIndex[2]);
-        }
-        else
-        {
-            // Probably a comment
-            char stupidBuffer[1000];
-            fgets(stupidBuffer, 1000, file);
-        }
-    }
-
-    cout << vertexIndices.size() << std::endl;
-
-    for (unsigned int i = 0; i < vertexIndices.size(); i++)
-    {
-        // Get the indices of its attributes
-        unsigned int vertexIndex = vertexIndices[i];
-        unsigned int uvIndex = uvIndices[i];
-        unsigned int normalIndex = normalIndices[i];
-
-        // Get the attributes thanks to the index
-        glm::vec3 vertex = temp_vertices[vertexIndex - 1];
-        glm::vec2 uv = temp_uvs[uvIndex - 1];
-        glm::vec3 normal = temp_normals[normalIndex - 1];
-
-        // Put the attributes in buffers
-        out_vertices.push_back(vertex); // std::cout<<vertex.x<< " " << vertex.y<< " " << vertex.z << "       ";
-
-        out_uvs.push_back(uv); // std::cout<< uv.x<<" " << uv.y << "       ";
-
-        out_normals.push_back(normal); // std::cout<<normal.x<<" " <<normal.y<<" "<<normal.z<<std::endl;
-    }
-
-    return true;
-}
 
 void framebuffer_size_callback(GLFWwindow *window, int width, int height);
 void processInput(GLFWwindow *window);
-
-vector<vec3> posion;
-vector<vec2> texture;
-vector<vec3> normals;
 
 // settings
 const unsigned int SCR_WIDTH = 800;
 const unsigned int SCR_HEIGHT = 600;
 
+// const char *vertexShaderSource = "#version 330 core\n"
+//                                  "layout(location = 0) in vec4 VertexPosition;\n"
+//                                  "layout(location = 1) in vec4 VertexColor;\n"
+//                                  "uniform mat4 MVP;\n"
+//                                  "out vec4 Color;\n" // Declare an output variable for color
+//                                  "void main()\n"
+//                                  "{\n"
+//                                  "   gl_Position = MVP * vec4(VertexPosition.xyz, 1.0);\n"
+//                                  "   Color = VertexColor;\n" // Pass the color to the fragment shader
+//                                  "}\0";
 const char *vertexShaderSource =
     "#version 330 core\n"
 
-    "layout(location = 0) in vec3 VertexPosition;\n"
-    "layout(location = 1) in vec3 VertexColor;\n"
+    "layout(location = 0) in vec3 inPosition;\n"
+    "layout(location = 1) in vec3 inColor;\n"
+    "layout(location = 2) in vec2 inTexCoord;\n"
+    "layout(location = 3) in int inFaceIndex;\n"
+
+    "out vec3 color;\n"
+    "out vec2 texCoord;\n"
+    "flat out int indexFace;\n"
+
     "uniform mat4 MVP;\n"
-    "out vec3 Vcolor;\n"
+
     "void main(){\n"
-    "gl_Position = MVP * vec4(VertexPosition, 1);\n"
-    "Vcolor = VertexColor;\n"
+    "gl_Position = MVP * vec4(inPosition, 1.0);\n"
+
+    "color = inColor;\n"
+    "texCoord = inTexCoord;\n"
+    "indexFace = inFaceIndex;\n"
     "}\0";
 
 const char *fragmentShaderSource =
     "#version 330 core\n"
 
-    "out vec3 color;\n"
-    "in vec3 Vcolor;\n"
+    "in vec3 color;\n"
+    "in vec2 texCoord;\n"
+    "flat in int indexFace;\n"
+
+    "out vec4 outColor;\n"
+
+    "uniform sampler2D OurTexture;\n"
 
     "void main()\n"
     "{\n"
-    "color = Vcolor;   //vec3(1,0,0);\n"
+    "vec2 combinedTexCoord = texCoord + vec2(float(indexFace), 0.0);\n"
+    "outColor = texture(OurTexture, combinedTexCoord);\n"
     "}\n\0";
 
 mat4 Model = mat4(1.0f);
@@ -203,8 +117,88 @@ static void key_callback(GLFWwindow *window, int key, int scancode, int action, 
         glClearColor(0.0f, 0.0f, 0.0f, 0.5f);
     }
 }
+
+struct STRVertex
+{
+    vec3 position;
+    vec3 color;
+    vec2 texCoord;
+    int indexFace;
+};
+
+GLuint textures[5]; 
+
+void InitTexture(void)
+{
+    char data[128 * 128 * 3];
+    FILE* f = fopen("textures/damier.raw", "rb");
+    if (f)
+    {
+        fread(data, 128 * 128 * 3, 1, f);
+        fclose(f);
+        glGenTextures(1, &textures[0]);
+        glBindTexture(GL_TEXTURE_2D, textures[0]);
+        glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, 128, 128, 0, GL_RGB, GL_UNSIGNED_BYTE, data);
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+    }
+
+
+    // Load texture for face 0
+    FILE* f0 = fopen("textures/1.raw", "rb");
+    fread(data, 128 * 128 * 3, 1, f0);
+    fclose(f0);
+    glGenTextures(1, &textures[1]);
+    glBindTexture(GL_TEXTURE_2D, textures[1]);
+    glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, 128, 128, 0, GL_RGB, GL_UNSIGNED_BYTE, data);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+
+    // Load texture for face 1
+    FILE* f1 = fopen("textures/2.raw", "rb");
+    fread(data, 128 * 128 * 3, 1, f1);
+    fclose(f1);
+    glGenTextures(1, &textures[2]);
+    glBindTexture(GL_TEXTURE_2D, textures[2]);
+    glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, 128, 128, 0, GL_RGB, GL_UNSIGNED_BYTE, data);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+
+    // Load texture for face 2
+    FILE* f2 = fopen("textures/3.raw", "rb");
+    fread(data, 128 * 128 * 3, 1, f2);
+    fclose(f2);
+    glGenTextures(1, &textures[3]);
+    glBindTexture(GL_TEXTURE_2D, textures[3]);
+    glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, 128, 128, 0, GL_RGB, GL_UNSIGNED_BYTE, data);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+
+    // Load texture for face 2
+    FILE* f3 = fopen("textures/4.raw", "rb");
+    fread(data, 128 * 128 * 3, 1, f3);
+    fclose(f3);
+    glGenTextures(1, &textures[4]);
+    glBindTexture(GL_TEXTURE_2D, textures[4]);
+    glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, 128, 128, 0, GL_RGB, GL_UNSIGNED_BYTE, data);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+
+    // Load texture for face 2
+    FILE* f4 = fopen("textures/5.raw", "rb");
+    fread(data, 128 * 128 * 3, 1, f4);
+    fclose(f4);
+    glGenTextures(1, &textures[5]);
+    glBindTexture(GL_TEXTURE_2D, textures[5]);
+    glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, 128, 128, 0, GL_RGB, GL_UNSIGNED_BYTE, data);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+};
+
 int main()
 {
+    // glfw: initialize and configure
+    // ------------------------------
     if (!glfwInit())
     {
         std::cout << "Failed to initialize GLFW" << std::endl;
@@ -239,6 +233,9 @@ int main()
         return -1;
     }
 
+    // build and compile our shader program
+    // ------------------------------------
+    // vertex shader
     unsigned int vertexShader = glCreateShader(GL_VERTEX_SHADER);
     glShaderSource(vertexShader, 1, &vertexShaderSource, NULL);
     glCompileShader(vertexShader);
@@ -284,77 +281,103 @@ int main()
     Model = scale(Model, vec3(2.5f, 1.5f, 1.0f));
     mat4 MVP = Projection * View * Model;
     GLuint MatrixID = glGetUniformLocation(shaderProgram, "MVP");
+    
 
-    loadOBJ("torus.obj", posion, texture, normals);
+    // set up vertex data (and buffer(s)) and configure vertex attributes
+    // ------------------------------------------------------------------
+    STRVertex vertices[] = {
+        // CarrÃ©
+        {vec3(1.0f, 0.0f, 1.0f), vec3(0.0f, 0.0f, 1.0f), vec2(1.0f, 1.0f), 0}, 
+        {vec3(-1.0f, 0.0f, 1.0f), vec3(0.0f, 0.0f, 1.0f), vec2(0.0f, 1.0f), 0},
+        {vec3(-1.0f, 0.0f, -1.0f), vec3(0.0f, 0.0f, 1.0f), vec2(0.0f, 0.0f), 0},
+        
 
-    unsigned int VBO, VAO;
-    unsigned int VBONormal;
-    // unsigned int  VBOTexture;
+        {vec3(-1.0f, 0.0f, -1.0f), vec3(0.0f, 0.0f, 1.0f), vec2(0.0f, 0.0f), 0},
+        {vec3(1.0f, 0.0f, -1.0f), vec3(0.0f, 0.0f, 1.0f), vec2(1.0f, 0.0f), 0},
+        {vec3(1.0f, 0.0f, 1.0f), vec3(0.0f, 0.0f, 1.0f), vec2(1.0f, 1.0f), 0},
+
+        // Triangle 1
+        {vec3(0.0f, 1.0f, 0.0f), vec3(1.0f, 0.0f, 0.0f), vec2(1.0f, 0.0f),1},
+        {vec3(-1.0f, 0.0f, 1.0f), vec3(1.0f, 0.0f, 0.0f), vec2(0.0f, 1.0f),1},
+        {vec3(-1.0f, 0.0f, -1.0f), vec3(1.0f, 0.0f, 0.0f), vec2(0.0f, 0.0f),1},
+
+        // Triangle 2
+        {vec3(0.0f, 1.0f, 0.0f), vec3(1.0f, 1.0f, 0.0f), vec2(1.0f, 1.0f), 2},
+        {vec3(1.0f, 0.0f, -1.0f), vec3(1.0f, 1.0f, 0.0f), vec2(0.0f, 0.0f), 2},
+        {vec3(-1.0f, 0.0f, -1.0f), vec3(1.0f, 1.0f, 0.0f), vec2(0.0f, 1.0f), 2},
+
+        // Triangle 3
+        {vec3(0.0f, 1.0f, 0.0f), vec3(0.0f, 1.0f, 0.0f), vec2(1.0f, 1.0f),3},
+        {vec3(1.0f, 0.0f, 1.0f), vec3(0.0f, 1.0f, 0.0f), vec2(0.0f, 0.0f),3},
+        {vec3(-1.0f, 0.0f, 1.0f), vec3(0.0f, 1.0f, 0.0f), vec2(1.0f, 0.0f),3},
+
+        // Triangle 4
+        {vec3(0.0f, 1.0f, 0.0f), vec3(0.0f, 1.0f, 1.0f), vec2(0.0f, 1.0f), 4},
+        {vec3(1.0f, 0.0f, -1.0f), vec3(0.0f, 1.0f, 1.0f), vec2(1.0f, 0.0f), 4},
+        {vec3(1.0f, 0.0f, 1.0f), vec3(0.0f, 1.0f, 1.0f), vec2(1.0f, 1.0f), 4},
+    };
+
+    unsigned int vertex_index []={0, 1, 2,
+                                2, 3, 1,
+                                4, 5, 2,
+                                4, 3, 2,
+                                4, 0, 1,
+                                4, 3, 1};
+
+
+    GLuint VAO;
+    GLuint VBO;
 
     glGenVertexArrays(1, &VAO);
-    glGenBuffers(1, &VBO);
     glBindVertexArray(VAO);
+    glGenBuffers(1, &VBO);
     glBindBuffer(GL_ARRAY_BUFFER, VBO);
-    glBufferData(GL_ARRAY_BUFFER, posion.size() * sizeof(vec3), &posion[0], GL_STATIC_DRAW);
 
-    // Correction de noms de fonctions OpenGL et indices de vertex attribs
-    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(GLfloat), (void *)0);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+    // Attribut 0 : position
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, sizeof(STRVertex), (void*)offsetof(STRVertex, position));
     glEnableVertexAttribArray(0);
 
-    glGenBuffers(1, &VBONormal);
-    glBindBuffer(GL_ARRAY_BUFFER, VBONormal);
-    glBufferData(GL_ARRAY_BUFFER, normals.size() * sizeof(vec3), &normals[0], GL_STATIC_DRAW);
+    // Attribut 2 : coordonnÃ©es de texture
+    glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, sizeof(STRVertex), (void*)offsetof(STRVertex, texCoord));
+    glEnableVertexAttribArray(2);
 
-    glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(GLfloat), (void *)0);
-    glEnableVertexAttribArray(1);
-    // note that this is allowed, the call to glVertexAttribPointer registered VBO as the vertex attribute's bound vertex buffer object so afterwards we can safely unbind
     glBindBuffer(GL_ARRAY_BUFFER, 0);
-
-    // You can unbind the VAO afterwards so other VAO calls won't accidentally modify this VAO, but we'll do so to keep things a bit more organized
     glBindVertexArray(0);
+
+    // Load the texture
+    GLuint textureID;
+    glGenTextures(1, &textureID);
+    glBindTexture(GL_TEXTURE_2D, textureID);
 
     glEnable(GL_DEPTH_TEST);
     glDepthFunc(GL_LESS);
 
-    processInput(window);
+    std::vector<vec3> position; 
+    std::vector<vec2> texture; 
+    std::vector<vec3> normals;
 
+    // render loop
+    // -----------
     glfwSetKeyCallback(window, key_callback);
     glfwSetCursorPosCallback(window, cursor_position);
     glfwSetScrollCallback(window, scroll);
-    // Declare global variables for camera
-    vec3 camPos = vec3(0.0f, 0.0f, 3.0f);
-    vec3 camDirection = vec3(0.0f, 0.0f, -1.0f);
-    vec3 camUp = vec3(0.0f, 1.0f, 0.0f);
-    vec3 camRight = vec3(1.0f, 0.0f, 0.0f);
-    float camSpeed = 0.1f; // Adjust the speed as needed
+    float radius = 5.0f;
+    float cameraSpeed = 1.0f;
+    InitTexture();
+    GLuint TextureID = glGetUniformLocation(shaderProgram, "OurTexture");
     while (!glfwWindowShouldClose(window))
     {
+
         glfwGetCursorPos(window, &Xpos, &Ypos);
         // std::cout << "La position du curseur : " << Xpos << " , " << Ypos << std::endl;
         // if ( glfwGetMouseButton(window, GLFW_MOUSE_BUTTON_LEFT) == GLFW_PRESS)
-        // std::cout << " Bouton gauche de la souris appuyÃ© " << std::endl;
+        // std::cout << " Bouton gauche de la souris appuyÃƒÂ© " << std::endl;
         //  Trackball camera
-        // FreeFly camera movement
-        if (glfwGetKey(window, GLFW_KEY_E) == GLFW_PRESS)
-        {
-            std::cout << "E key pressed!" << std::endl;
-            camPos += camSpeed * camDirection;
-        }
-
-        if (glfwGetKey(window, GLFW_KEY_S) == GLFW_PRESS)
-        {
-            std::cout << "S key pressed!" << std::endl;
-            camPos -= camSpeed * camDirection;
-        }
-        if (glfwGetKey(window, GLFW_KEY_X) == GLFW_PRESS)
-            camPos -= camSpeed * camRight;
-        if (glfwGetKey(window, GLFW_KEY_D) == GLFW_PRESS)
-            camPos += camSpeed * camRight;
-
-        // Update the View matrix
-        View = lookAt(camPos, camPos + camDirection, camUp);
-
-        // Update the MVP matrix
+        float cameraX = radius * sin(glfwGetTime() * cameraSpeed);
+        float cameraZ = radius * cos(glfwGetTime() * cameraSpeed);
+        View = lookAt(vec3(cameraX, 0.0, cameraZ), vec3(0, 0, 0), vec3(0, 1, 0));
         mat4 MVP = Projection * View * Model;
         glUniformMatrix4fv(MatrixID, 1, GL_FALSE, &MVP[0][0]);
 
@@ -362,13 +385,33 @@ int main()
 
         glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
+
         // draw our first triangle
         glUseProgram(shaderProgram);
-        glBindVertexArray(VAO);
+        //glBindVertexArray(VAO);
         glUniformMatrix4fv(MatrixID, 1, GL_FALSE, &MVP[0][0]);
         glBindVertexArray(VAO);
-        glDrawArrays(GL_TRIANGLES, 0, posion.size());
+        glActiveTexture(GL_TEXTURE0);
+        glUniform1i(TextureID, 0);
+
+        for (int i = 0; i < 6; ++i) {
+            glActiveTexture(GL_TEXTURE0);
+            glBindTexture(GL_TEXTURE_2D, textures[vertices[i * 3].indexFace]);
+            glUniform1i(TextureID, 0);
+
+            glDrawArrays(GL_TRIANGLES, i * 3, 3);
+            
+        }
+
+        glUniform1i(TextureID, 0);
+        glDrawArrays(GL_TRIANGLES, 0, 18); // Assuming 18 vertices for the entire geometry
+
+
+        glDrawArrays(GL_TRIANGLES, 0, 18);
+        //glDrawElements(GL_TRIANGLES, 18, GL_UNSIGNED_INT, (void*)0);
+
         glfwSwapBuffers(window);
+
         glfwPollEvents();
     }
 
